adf820f78506b3e1f0b7718f3d37d19fa814b394
David Suarez <dsuarey@gmail.com>
2018-08-23 14:35:54 -0700

    Created everything

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("JS loaded");
+
+// When a user types in a question, they have to CLICK enter to receive an answer
+// An answer will be chosen at random
+// The answer will be in the dark blue circle 
+// After hitting enter the answer should say either: "Yes", "No, sorry", or "Possibly"
+
+
+
+document.querySelector("form").addEventListener("submit", submitMe);
+
+
+function submitMe(e) {
+    e.preventDefault();
+    var answersArr = ["HELL Yes!", "Hell NO!", "uh Prolly."];
+    var randomArr = answersArr[Math.floor(answersArr.length * Math.random())];
+    document.querySelector(".answer").innerText = randomArr;
+ 
+}
+document.getElementsByClassName("reset.btn").reset( function() {
+    
+}); 
+
+
